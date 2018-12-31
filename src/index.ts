772bfcfbea86e818e873e3dec906589883fe7efc
dblum <dennis.blum.1989@googlemail.com>
2018-12-31 08:13:00 +0100

    export all models

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ import { PublicTransportService } from './PublicTransportService';
 const publicTransportService = new PublicTransportService();
 
 export { Network } from './common/models/Network';
+export { Departure } from './common/models/Departure';
+export { Line } from './common/models/Line';
+export { LocationType } from './common/models/LocationType';
+export { Station } from './common/models/Station';
+
 export default publicTransportService;
